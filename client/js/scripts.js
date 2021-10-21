90ef707b95849966b28a2fd5448298bf589e1b15
Hardik Jain <jainhardik2626.hj26@gmail.com>
2021-10-22 01:24:10 +0530

    few changes made

---
+++ client/js/scripts.js
@@ -0,0 +1,75 @@
+var zodiacs = [
+{
+	name:"Aries", 
+    month:"March 21 - April 20",
+    description:"Aries loves to be number one, so it’s no surprise that these audacious rams are the first sign of the zodiac. Bold and ambitious, Aries dives headfirst into even the most challenging situations." 
+
+},
+{
+	name:"Taurus",
+	month:"April 21 - May20",
+	description:"Taurus is an earth sign represented by the bull. Like their celestial spirit animal, Taureans enjoy relaxing in serene, bucolic environments surrounded by soft sounds, soothing aromas, and succulent flavors."
+},
+{
+	name:"Gemini"
+	month:"May 21 - June 20",
+	description:"Have you ever been so busy that you wished you could clone yourself just to get everything done? That’s the Gemini experience in a nutshell. Appropriately symbolized by the celestial twins, this air sign was interested in so many pursuits that it had to double itself."
+
+},
+{
+	name:"Cancer",
+	month:"June 21 - July 20",
+	description:"Cancer is a cardinal water sign. Represented by the crab, this crustacean seamlessly weaves between the sea and shore representing Cancer’s ability to exist in both emotional and material realms. Cancers are highly intuitive and their psychic abilities manifest in tangible spaces: For instance, Cancers can effortlessly pick up the energies in a room."
+},
+
+{
+	name:"Leo",
+	month:"July 21 - August 20",
+	description:"Roll out the red carpet because Leo has arrived. Leo is represented by the lion and these spirited fire signs are the kings and queens of the celestial jungle. They’re delighted to embrace their royal status: Vivacious, theatrical, and passionate, Leos love to bask in the spotlight and celebrate themselves."
+
+},
+
+{
+    name:"Virgo",
+    month:"August 21- September 20",
+    description:"Virgo is an earth sign historically represented by the goddess of wheat and agriculture, an association that speaks to Virgo’s deep-rooted presence in the material world. Virgos are logical, practical, and systematic in their approach to life. This earth sign is a perfectionist at heart and isn’t afraid to improve skills through diligent and consistent practice."
+},
+
+{
+	name:"Libra",
+	month:"September 21 - October 20",
+	description:"Libra is an air sign represented by the scales (interestingly, the only inanimate object of the zodiac), an association that reflects Libra's fixation on balance and harmony. Libra is obsessed with symmetry and strives to create equilibrium in all areas of life."
+},
+
+{
+	name:"Scorpio",
+	month:"October 21 - November 20",
+	description:"Scorpio is one of the most misunderstood signs of the zodiac. Because of its incredible passion and power, Scorpio is often mistaken for a fire sign. In fact, Scorpio is a water sign that derives its strength from the psychic, emotional realm."
+},
+
+{
+
+	name:"Sagittarius",
+	month:"November 21 - December 20",
+	description:"Represented by the archer, Sagittarians are always on a quest for knowledge. The last fire sign of the zodiac, Sagittarius launches its many pursuits like blazing arrows, chasing after geographical, intellectual, and spiritual adventures."
+},
+
+{
+	name:"Capricon",
+	month:"December 21 - January 20",
+	description:"The last earth sign of the zodiac, Capricorn is represented by the sea goat, a mythological creature with the body of a goat and the tail of a fish. Accordingly, Capricorns are skilled at navigating both the material and emotional realms."
+},
+
+{
+	name:"Aquarius",
+	month:"January 21 - February 18",
+	description:"Despite the “aqua” in its name, Aquarius is actually the last air sign of the zodiac. Aquarius is represented by the water bearer, the mystical healer who bestows water, or life, upon the land. Accordingly, Aquarius is the most humanitarian astrological sign."
+},
+
+{
+	name:"Pisces",
+	month:"febuary 19 - March 20",
+	description:"Pisces, a water sign, is the last constellation of the zodiac. It's symbolized by two fish swimming in opposite directions, representing the constant division of Pisces' attention between fantasy and reality. As the final sign, Pisces has absorbed every lesson — the joys and the pain, the hopes and the fears — learned by all of the other signs."
+},
+
+]
